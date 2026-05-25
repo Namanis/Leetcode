4d81d68fd795c59455f3033187ce8f4bd24dc510
MykolasSkrodenis <mykolas.skrodenis@gmail.com>
2026-05-25 18:54:33 +0300

    Palindrome

---
+++ Leetcode/arrays/Palindrome/Main.java
@@ -0,0 +1,14 @@
+package Leetcode.arrays.Palindrome;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        Boolean result = solution.isPalindrome(998765432);
+
+        System.out.println(result);
+    }
+    
+}

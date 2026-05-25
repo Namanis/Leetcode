6643800c5fb6c4155278f8eb58458126d7c82650
MykolasSkrodenis <mykolas.skrodenis@gmail.com>
2026-05-25 22:46:19 +0300

    Valid Parentheses

---
+++ Leetcode/Easy/Valid_Parentheses/Main.java
@@ -0,0 +1,12 @@
+package Leetcode.Easy.Valid_Parentheses;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        Boolean result = solution.isValid("([])");
+
+        System.out.println((result));
+    }
+}

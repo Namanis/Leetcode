a6951da2b5420266e6d195629f0faf6791fbb5d7
MykolasSkrodenis <mykolas.skrodenis@gmail.com>
2026-06-01 15:04:16 +0300

    Remove Element

---
+++ Leetcode/Easy/Remove_Element/Main.java
@@ -0,0 +1,11 @@
+package Leetcode.Easy.Remove_Element;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int result = solution.removeElement(new int[]{3,2,2,3}, 3);
+
+        System.out.println(result);
+    }
+}

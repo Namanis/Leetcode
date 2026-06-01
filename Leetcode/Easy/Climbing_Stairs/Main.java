25fb21088d41f3055fa7a3f675700359ed13bd53
MykolasSkrodenis <mykolas.skrodenis@gmail.com>
2026-06-01 12:36:40 +0300

    climbStairs

---
+++ Leetcode/Easy/Climbing_Stairs/Main.java
@@ -0,0 +1,13 @@
+package Leetcode.Easy.Climbing_Stairs;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+
+
+        int result = solution.climbStairs(35);
+
+        System.out.println(result);
+    }
+}
